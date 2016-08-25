d5ac31d6208e8dc5e26cd60a5d9154b3e5e6255f
James Sherriff <jamessherriff158@gmail.com>
2016-08-25 14:40:14 -0400

    Thing

--- public/game.js
+++ public/game.js
@@ -146,7 +146,7 @@ var render = function () {
             console.log("Drawing hero " + other_hero + " at " + other_heroes[other_hero].x + " " + other_heroes[other_hero].y);
             ctx.drawImage(heroImage, other_heroes[other_hero].x, other_heroes[other_hero].y);
             ctx.font = "10px Helvetica";
-            ctx.fillText(other_heroes[other_hero].username, other_heroes[other_hero].x, other_heroes[other_hero].y - 32);
+            ctx.fillText(other_heroes[other_hero].username, other_heroes[other_hero].x, other_heroes[other_hero].y - 20);
             ctx.font = "24px Helvetica";
             ctx.fillText("Users: " + other_heroes[other_hero].username, 32, 32 * (i+1));
         }
