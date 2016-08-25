d48186a7fd8b09b00487527fc470980bf1ac77f8
James Sherriff <jamessherriff158@gmail.com>
2016-08-25 14:35:34 -0400

    Font size changes

--- public/game.js
+++ public/game.js
@@ -145,7 +145,7 @@ var render = function () {
             i++;
             console.log("Drawing hero " + other_hero + " at " + other_heroes[other_hero].x + " " + other_heroes[other_hero].y);
             ctx.drawImage(heroImage, other_heroes[other_hero].x, other_heroes[other_hero].y);
-            ctx.font = "10px Helvetica";
+            ctx.font = "24px Helvetica";
             ctx.fillText(other_heroes[other_hero].username, other_heroes[other_hero].x, other_heroes[other_hero].y + 12);
             ctx.font = "24px Helvetica";
             ctx.fillText("Users: " + other_heroes[other_hero].username, 32, 32 * (i+1));
