668a0c6d3df630718079985630c7749b85dcfd44
James Sherriff <jamessherriff158@gmail.com>
2016-08-25 13:57:45 -0400

    Bug

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ var goblin_y = 32 + (Math.random() * (480 - 64));
 // Socket.io
 io.on('connection', function (socket) {
     var user_id = Math.random();
+    var username = null;
     socket.emit('client_setup', {id: user_id, users: connected_users, goblins: goblins_caught, goblin_x: goblin_x, goblin_y: goblin_y});
     connected_users[user_id] = {x: 256, y: 240};
     io.sockets.emit('hero_update', {id: user_id, x: 256, y: 240});
@@ -30,9 +31,9 @@ io.on('connection', function (socket) {
     });
     socket.on('update_hero', function(data){
         console.log("Recieved a hero update");
-        connected_users[data.id] = {x: data.x, y: data.y};
+        connected_users[user_id] = {username: username, x: data.x, y: data.y};
         console.log("Sending hero location updates");
-        io.sockets.emit('hero_update', {id: data.id, username: connected_users[data.id].username, x: data.x, y: data.y});
+        io.sockets.emit('hero_update', {id: user_id, username: username, x: data.x, y: data.y});
     });
     socket.on('goblin_caught', function() {
         console.log("Goblin caught by " + user_id + "!");
