8b250a2250222b082f74238775c29908343e9cd3
James Sherriff <jamessherriff158@gmail.com>
2016-09-01 00:51:15 +0100

    Rounded player pos

--- public/game.js
+++ public/game.js
@@ -96,35 +96,23 @@ var update = function (modifier) {
     heroDelta = {x: 0, y: 0};
 	if (38 in keysDown) { // Player holding up
 		hero.y -= hero.speed * modifier;
-        heroDelta.y += hero.speed * modifier;
-        if(heroDelta.y >= 1){
-            socket.emit('update_hero', {id: user_id, x: hero.x,y: hero.y});  
-            heroDelta.y = 0;
-        }
+        hero.y = Math.round(hero.y);
+        socket.emit('update_hero', {id: user_id, x: hero.x,y: hero.y});  
 	}
 	if (40 in keysDown) { // Player holding down
 		hero.y += hero.speed * modifier;
-        heroDelta.y += hero.speed * modifier;
-        if(heroDelta.y >= 1){
-            socket.emit('update_hero', {id: user_id, x: hero.x,y: hero.y});  
-            heroDelta.y = 0;
-        }
+        hero.y = Math.round(hero.y);
+        socket.emit('update_hero', {id: user_id, x: hero.x,y: hero.y});  
     }
 	if (37 in keysDown) { // Player holding left
 		hero.x -= hero.speed * modifier;
-        heroDelta.x += hero.speed * modifier;
-        if(heroDelta.x >= 1){
-            socket.emit('update_hero', {id: user_id, x: hero.x,y: hero.y}); 
-            heroDelta.x = 0;
-        }
+        hero.x = Math.round(hero.x);
+        socket.emit('update_hero', {id: user_id, x: hero.x,y: hero.y}); 
 	}
 	if (39 in keysDown) { // Player holding right
 		hero.x += hero.speed * modifier;
-        heroDelta.x += hero.speed * modifier;
-        if(heroDelta.x >= 1){
-            socket.emit('update_hero', {id: user_id, x: hero.x,y: hero.y}); 
-            heroDelta.x = 0;
-        }
+        hero.x = Math.round(hero.x);
+        socket.emit('update_hero', {id: user_id, x: hero.x,y: hero.y}); 
         
 	}
     
